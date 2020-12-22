7579299735392a9b5a31785a0e91dd32731f7fd4
cecile gaubert <primaelswann@gmail.com>
2020-12-22 22:05:00 +0100

    finition login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,41 +12,28 @@ const { createUser, findOne } = require('../models/User');
 exports.signup = async (req, res, next) => {
     console.log('ok', req.body)
     const hash = await bcrypt.hash(req.body.password, 10)
-    
+
     const newUser = await createUser(req.body, hash)
-    if (newUser.affectedRows >0) {
-       return res.status(201).send(' utilisateur créé ! ')
+    if (newUser.affectedRows > 0) {
+        return res.status(201).send(' utilisateur créé ! ')
     }
     else {
         res.status(500).send('error')
-    }       
+    }
 };
 
 exports.login = async (req, res, next) => {
-    console.log('ok', req.body)
     const user = await findOne(req.body.email)
     console.log(user)
-
-    /*User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                    }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));*/
+    if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+    res.status(200).json({
+        userId: user._id,
+        token: jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+        )
+    })
 };
